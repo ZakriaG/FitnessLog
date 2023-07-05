a9287d62010afeea59a357a3ad358ec6270ebb7b
AZG0404 <79763101+AZG0404@users.noreply.github.com>
2023-07-05 21:45:33 +0200

    fixed .env path

--- FitnessLog/settings.py
+++ FitnessLog/settings.py
@@ -13,9 +13,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = Path(BASE_DIR, '.env')
+load_dotenv(dotenv_path=env_path)
 
 
 # Quick-start development settings - unsuitable for production
