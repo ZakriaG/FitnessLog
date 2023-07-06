35418046de019ebca0c96a23763a7add58ab785b
AZG0404 <79763101+AZG0404@users.noreply.github.com>
2023-07-06 22:15:20 +0200

    workout subpages added

--- workout/urls.py
+++ workout/urls.py
@@ -4,5 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.myview),
     path('meal', views.meal, name="meal"),
-    path('workout', views.workout, name="workout")
+    path('workout', views.workout, name="workout"),
+    path('workout/monday', views.workout_monday, name="workout_monday"),
+    path('tuesday', views.workout_tuesday, name="workout_tuesday"),
+    path('workout', views.workout_tuesday, name="workout_wednesday"),
+    path('workout', views.workout_thursday, name="workout_thursday"),
+    path('workout', views.workout_friday, name="workout_friday"),
+    path('workout', views.workout_saturday, name="workout_saturday"),
+    path('workout', views.workout_sunday, name="workout_sunday")
+
 ]
--- workout/views.py
+++ workout/views.py
@@ -9,3 +9,24 @@ def meal(request):
 
 def workout(request):
     return render(request, "./workout/index.html")
+
+def workout_monday(request):
+    return render(request, "./workout/monday.html")
+
+def workout_tuesday(request):
+    return render(request, "./workout/tuesday.html")
+
+def workout_wednesday(request):
+    return render(request, "./workout/wednesday.html")
+
+def workout_thursday(request):
+    return render(request, "./workout/thursday.html")
+
+def workout_friday(request):
+    return render(request, "./workout/friday.html")
+
+def workout_saturday(request):
+    return render(request, "./workout/Saturday.html")
+
+def workout_sunday(request):
+    return render(request, "./workout/sunday.html")
