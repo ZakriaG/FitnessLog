dfbce15ddf87304779b7cfc0f20fbcb66640f36d
AZG0404 <79763101+AZG0404@users.noreply.github.com>
2023-07-09 22:26:04 +0200

    tuesday

--- workout/urls.py
+++ workout/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('meal', views.meal, name="meal"),
     path('workout', views.workout, name="workout"),
     path('workout/monday', views.workout_monday, name="workout_monday"),
-    path('tuesday', views.workout_tuesday, name="workout_tuesday"),
-    path('workout', views.workout_tuesday, name="workout_wednesday"),
-    path('workout', views.workout_thursday, name="workout_thursday"),
-    path('workout', views.workout_friday, name="workout_friday"),
-    path('workout', views.workout_saturday, name="workout_saturday"),
-    path('workout', views.workout_sunday, name="workout_sunday")
+    path('workout/tuesday', views.workout_tuesday, name="workout_tuesday"),
+    path('workout/wednesday', views.workout_wednesday, name="workout_wednesday"),
+    path('workout/thursday', views.workout_thursday, name="workout_thursday"),
+    path('workout/friday', views.workout_friday, name="workout_friday"),
+    path('workout/saturday', views.workout_saturday, name="workout_saturday"),
+    path('workout/sunday', views.workout_sunday, name="workout_sunday")
 ]
