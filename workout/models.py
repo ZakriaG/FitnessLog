082c7b5c22739478ade76cc45784393502e86f1d
AZG0404 <79763101+AZG0404@users.noreply.github.com>
2023-07-31 21:58:30 +0200

    Database for workout log

--- workout/models.py
+++ workout/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+class WorkOutLog(models.Model):
+    # name of the exercise:
+    name = models.CharField(max_length=200)
+    # Weight in kg:
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return self.name
