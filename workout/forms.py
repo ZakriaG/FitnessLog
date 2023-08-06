4e7c842505f622f4f7bf05df609ebd2eb6f72d55
AZG0404 <79763101+AZG0404@users.noreply.github.com>
2023-08-06 20:47:17 +0200

    getting kg data from form

---
+++ workout/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+
+class WorkoutLog(forms.Form):
+    ExerciseName = forms.CharField(max_length=200)
+    Set1Weight = forms.IntegerField()
+    Set2Weight = forms.IntegerField()
+    Set3Weight = forms.IntegerField()
+    Set4Weight = forms.IntegerField()
--- workout/views.py
+++ workout/views.py
@@ -1,32 +1,62 @@
 from django.shortcuts import render
+from .forms import WorkoutLog
 
 # Create your views here.
 def myview(request):
     return render(request, "./home/index.html")
 
+
 def meal(request):
     return render(request, "./meal/index.html")
+
 
 def workout(request):
     return render(request, "./workout/baseWorkout.html")
 
-def workout_monday(request):
-    return render(request, "./workout/monday.html")
+
+def workout_monday(response):
+    if response.method == "POST":
+        form = WorkoutLog(response.POST)
+        print("triggered")
+        print(f"n={form}")
+        #n = form.cleaned_data["weight"]ExerciseName
+        ExerciseName = form.cleaned_data["ExerciseName"]
+        Set1Weight = form.cleaned_data["Set1Weight"]
+        Set2Weight = form.cleaned_data["Set2Weight"]
+        Set3Weight = form.cleaned_data["Set3Weight"]
+        Set4Weight = form.cleaned_data["Set4Weight"]
+        print(f"ExerciseName={ExerciseName}")
+        print(f"n={Set2Weight}")
+        print(f"n={Set3Weight}")
+        print(f"n={Set4Weight}")
+        if form.is_valid():
+            w = WorkOutLog(name=n, nweight=w)
+            w.save()
+        #return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = WorkoutLog()
+    return render(response, "./workout/monday.html", {"form": form})
+
 
 def workout_tuesday(request):
     return render(request, "./workout/tuesday.html")
 
+
 def workout_wednesday(request):
     return render(request, "./workout/wednesday.html")
+
 
 def workout_thursday(request):
     return render(request, "./workout/thursday.html")
 
+
 def workout_friday(request):
     return render(request, "./workout/friday.html")
 
+
 def workout_saturday(request):
     return render(request, "./workout/Saturday.html")
+
 
 def workout_sunday(request):
     return render(request, "./workout/sunday.html")
